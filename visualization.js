9eb805f1908c57a07eb6eec953a2201e9eebd357
Brandon Chan <14073064d@connect.polyu.hk>
2017-02-13 13:53:11 +0800

    Add charts.html to visualize the data retrieved using pie and bar charts

--- visualization.js
+++ visualization.js
@@ -71,35 +71,31 @@ function getPageFanInfo(pageName, pageId, accessToken) {
         var values = (response.data[0] == null) ? null : response.data[0].values;
         if (values != null) {
             var stat = values[values.length-1].value; // get the latest statistic
-            var statTableHTML = "<div class='row'><div class='col s12 card-panel'><h5 class='center-align blue-text'>" + pageName + "</h5><table class='centered bordered'><thead>" +
-                "<tr><th></th><th>13-17</th><th>18-24</th><th>25-34</th><th>35-44</th><th>45-54</th><th>55-64</th><th>65+</th><th>Total</th></tr></thead><tbody>";
+            $('#pagesFanInfo').append("<div class='row'><div class='col s12 card-panel'><h5 class='center-align blue-text'>" + pageName + "</h5>");
             var formattedStat = {
                 "Female": [0,0,0,0,0,0,0],
                 "Male": [0,0,0,0,0,0,0],
                 "Unknown": [0,0,0,0,0,0,0],
             }; // declare an object to store the number of fans by gender by age groups
             var totalFans = [0,0,0,0,0,0,0]; // total numbers of fans by age groups
-            var ageGroupPos = ["13-17", "18-24", "25-34", "35-44", "45-54", "55-64", "65+"];
+            var ageGroupLabels = ["13-17", "18-24", "25-34", "35-44", "45-54", "55-64", "65+"];
             for (var key in stat) {
                 if (stat.hasOwnProperty(key)) {
                     var gender = getFullGender(key.split(".")[0]);
                     var ageGroup = key.split(".")[1];
                     var numOfFans = stat[key];
-                    var ageGroupIdx = ageGroupPos.indexOf(ageGroup);
+                    var ageGroupIdx = ageGroupLabels.indexOf(ageGroup);
                     formattedStat[gender][ageGroupIdx] = numOfFans; // by gender by age group
                     totalFans[ageGroupIdx] += numOfFans; // by age group
                 }
             }
             for (var genderKey in formattedStat) {
-                statTableHTML += "<tr><td>" + genderKey + "</td>";
+                var newCanvas = $('<canvas/>');
+                $('#pagesFanInfo').append('<div class="col s12 m6 l4"><h5>' + genderKey + '</h5>');
+                $('#pagesFanInfo').append(newCanvas);
                 var fansByAgeGroups = formattedStat[genderKey];
-                for (var i = 0; i < fansByAgeGroups.length; i++) {
-                    statTableHTML += "<td>" + fansByAgeGroups[i] + "</td>";
-                }
-                var totalFans = fansByAgeGroups.reduce(function(a, b) { return a + b; }, 0);
-                statTableHTML += "<td>" + totalFans + "</tr>";
+                createCharts(newCanvas, 'pie', fansByAgeGroups, {});
             }
-            $('#pagesFanInfo').append(statTableHTML + "</tbody></table></div>"); // render the table to the page
         }
     });
 }
@@ -110,9 +106,9 @@ function getFullGender(shortForm) {
     else return "Unknown";
 }
 
-function createCharts(ctx, data, option) {
+function createCharts(ctx, type, data, option) {
     var myChart = new Chart(ctx, {
-        type: 'bar',
+        type: type,
         data: data,
         options: option
     });
